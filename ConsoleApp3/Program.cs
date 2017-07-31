4008fa9111f4d0d95098bbeec090956cb96bad3d
hsip24 <shanaka.indika@gmail.com>
2017-07-31 17:40:41 +0530

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp3
         {
             Console.WriteLine("Hello shanaka");
             Console.WriteLine("Hello Perera");
+            Console.WriteLine("Hi");
         }
     }
 }
